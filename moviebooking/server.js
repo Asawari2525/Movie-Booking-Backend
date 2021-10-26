4952ceb45954930cfd64885db02e2f0821231b9c
Asawari2525 <baishflypi255@gmail.com>
2021-10-26 15:45:45 +0530

    Checkpoint 7

--- moviebooking/server.js
+++ moviebooking/server.js
@@ -1,5 +1,8 @@
 const http = require('http');
-const port = 9000;
+const app = require('./app');
+
+const PORT = process.env.PORT || 8085;
+const server = http.createServer(app);
 
 const app = http.createServer((req,res)=>{
     if(req.url === '/movies' && req.method === 'GET'){
@@ -17,17 +20,6 @@ const app = http.createServer((req,res)=>{
     }
 })
 const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-    
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
-app.listen(port);
+server.listen(PORT,()=>{
+  console.log(`Server listening on port ${PORT}`);
+});
